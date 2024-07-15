041c86c3f60b332d6dcd9fcda2205c22201b6bab
Sanjay <98222052+sanjayramu28@users.noreply.github.com>
2024-07-15 15:51:11 +0530

    Update Wrapper.java

--- Wrapper.java
+++ Wrapper.java
@@ -1,3 +1,26 @@
-public class Wrapper {
-    
+import java.util.*;
+public class Wrapper
+{
+	public static void main(String[] args) {
+	    int a=10;
+	    Integer obj1=a;
+	   
+	    char ch='d';
+	    Character obj2=ch;
+	    
+	    float f=10.5f;
+	    Float  obj3=f;
+	    
+	    
+	    boolean b1 = Boolean.parseBoolean("True");
+        Boolean obj4=b1;
+        
+        short s=100;
+        Short obj5=s;
+        
+        byte b= 6;
+        Byte obj6= b;
+        
+        System.out.print(obj6);
+	}
 }
