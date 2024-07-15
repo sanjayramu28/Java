2140c950f4329096e07186e1aca1a4b86e8e715b
Sanjay <email@example.com>
2024-07-15 15:28:35 +0530

    Pattern,EvenLetter,Wrapper

--- FirstLetter.java
+++ FirstLetter.java
@@ -5,7 +5,7 @@ public class FirstLetter{
     public static void main(String arg[]){
         Scanner sc=new Scanner(System.in);
         String s=sc.nextLine();
-        System.out.print(s.charAt(0));
+        System.out.print(s.charAt(0)+" ");
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)==' '){
                 System.out.print(s.charAt(i+1)+" ");
